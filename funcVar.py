ac578a517ab8fccd2c7a47229720313c322c3640
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-01-22 14:35:35 +0300

    added three files

---
+++ funcVar.py
@@ -0,0 +1,10 @@
+#  def hello():
+#    print("Whats up?")
+
+
+#  hi = hello
+#  hello()
+#  hi()
+
+say = print
+say("Function to variable")
---
+++ lambda.py
@@ -0,0 +1,6 @@
+#  Lambda. function written in 1 line using lambda keyword
+#  Has only one expression
+double = lambda x, y: x * y
+print(double(5, 2))
+add = lambda x, y, z: x + y + z
+print(add(3, 56, 5))
