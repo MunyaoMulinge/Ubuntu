0b16ae6a4174bfb961c4ca85a547fce78ece2548
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-01-26 14:25:31 +0300

    update dictionary

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dict1 = {
+    "f_name": "Munyao",
+    "l_name": "Mulinge",
+    "marital status": "Complicated"
+}
+dict2 = {
+    "club": "Koma City Stars FC",
+    "position": 8,
+    "contract": False
+}
+dict2.update(dict1)
+print(dict2)
+
+
