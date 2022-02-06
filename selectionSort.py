c93b8be2e2baccd617f4de15688ec2eb01cbc042
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-06 23:02:29 +0300

    selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,12 +1,22 @@
-a = [5, 2, 8, 14, 1, 16]
+import sys
 
+A = [64, 25, 12, 22, 11]
 
-def selectionSort(a):
-    n = len(a)
-    for k in xrange(n):
-        minimal = k
-        for j in xrange(k + 1, n):
-            if A[j] < A[minimal]:
-            minimal = j
-        A[k], A[minimal] = A[minimal], A[k]  # swap A[k] and A[minimal]
-    return A
+# Traverse through all array elements
+for i in range(len(A)):
+
+    # Find the minimum element in remaining 
+    # unsorted array
+    min_idx = i
+    for j in range(i + 1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+
+    # Swap the found minimum element with 
+    # the first element        
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+# Driver code to test above
+print("Sorted array")
+for i in range(len(A)):
+    print("%d" % A[i]),
